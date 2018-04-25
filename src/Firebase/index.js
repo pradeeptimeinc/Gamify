9e886d47b5f62999ef011a3b198af692ecda201d
kottalap <prashantkumar.kottalagi@timeinc.com>
2018-04-25 18:31:51 +0530

    Update index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,11 +1,11 @@
 import * as firebase from "firebase";
 const config = {
-  apiKey: "AIzaSyD4SmNBTYujsvMy79DowpPAEDNuvvRXMDk",
-  authDomain: "employee-engagement-14c56.firebaseapp.com",
-  databaseURL: "https://employee-engagement-14c56.firebaseio.com",
-  projectId: "employee-engagement-14c56",
-  storageBucket: "employee-engagement-14c56.appspot.com",
-  messagingSenderId: "78258854825"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 firebase.initializeApp(config);
 export default firebase;
