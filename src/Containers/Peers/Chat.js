2eeac9d1c9691d5174a3983dc59165975dba4a19
kottalap <prashantkumar.kottalagi@timeinc.com>
2018-04-27 03:32:08 +0530

    chat box

--- src/Containers/Peers/Chat.js
+++ src/Containers/Peers/Chat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, TextInput, FlatList, Image, ActivityIndicator } from 'react-native';
-import AddIcon from 'react-native-vector-icons/MaterialIcons'
+import AddIcon from 'react-native-vector-icons/MaterialIcons' 
 import moment from 'moment'
 import { connect } from 'react-redux'
 import firebase from '../../Firebase'
@@ -17,38 +17,58 @@ class Chat extends React.Component {
     super(props);
     this.state = {
       messages: [],
-      initailLoad: true,
+      recivedMessage: [],
+      initailLoad:true,
       text: ''
     }
   }
 
   componentDidMount() {
-    console.log("did mount....", this.props.user.id, this.props.navigation.state.params.id)
     const db = firebase.database();
     db.ref('/messages').orderByChild('sender').equalTo(this.props.user.id).once('value', (snapshot) => {
-      console.log('waejhvwhge', (Object.values(snapshot.val())))
-      if (Object.values(snapshot.val())) {
+      if(Object.values(snapshot.val())){
         let message = Object.values(snapshot.val());
-        message = _.filter(message, ['recipent', this.props.navigation.state.params.id])
-        console.log('final obj', message);
+        message=_.filter(message, ['recipent', this.props.navigation.state.params.id])
         this.setState({
-          messages: message,
-          initailLoad: false
+         messages: message,
+         initailLoad : false
+        })
+      }
+    });
+    db.ref('/messages').orderByChild('sender').equalTo(this.props.navigation.state.params.id).once('value', (snapshot) => {
+      if(Object.values(snapshot.val())){
+        let recived = Object.values(snapshot.val());
+        recived=_.filter(recived, ['recipent', this.props.user.id])
+        this.setState({
+         recivedMessage: recived,
+         initailLoad : false
         })
       }
     });
     db.ref('/messages').orderByChild('sender').equalTo(this.props.user.id).on('child_added', this.onItemAdded)
+    db.ref('/messages').orderByChild('sender').equalTo(this.props.navigation.state.params.id).on('child_added', this.onrecivedItemAdded)
   }
-  onItemAdded = (snapshot) => {
-    const { messages, initailLoad } = this.state;
-    if (!initailLoad) {
-      const newmgs = [snapshot.val(), ...messages];
+
+  onItemAdded =  (snapshot)=>{
+    const {messages, initailLoad}  = this.state;
+    if(!initailLoad){
+       const newmgs = [snapshot.val(),...messages];
       this.setState({
         messages: newmgs
       })
-    }
-
+    } 
   }
+
+  onrecivedItemAdded = (snapshot)=>{
+    const {recivedMessage, initailLoad}  = this.state;
+    if(!initailLoad){
+       const newrecivedmgs = [snapshot.val(),...recivedMessage];
+      this.setState({
+        recivedMessage: newrecivedmgs
+      })
+    }   
+  }
+
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state;
 
@@ -58,59 +78,60 @@ class Chat extends React.Component {
   };
 
   renderItem = ({ item }) => {
-    console.log("dataa", item)
-    return (
-      <View style={{ flex: 3 }}>
-        <Text style={{ fontSize: 16, alignItems: 'center', margin: 5 }}>{item.message}</Text>
-        <View style={{ flexDirection: "row", alignItems: 'flex-end', height: 30 }}>
-          <Text style={{ flex: 3, fontSize: 12, fontWeight: 'bold', color: '#bab8b8' }}>{item.sender}</Text>
-          <Text style={{ flex: 4, fontSize: 12, alignSelf: 'flex-end', color: '#444343' }}>{moment(item.timestamp).format('MMMM Do, h:mm:ss a')}</Text>
+    console.log("propsdata", this.props.user.id, item)
+    return (this.props.user.id === item.sender) ?  (
+      < View style={{ backgroundColor: 'white', flexDirection: "row", justifyContent: 'flex-start', margin: 3, borderTopRightRadius:30, borderBottomRightRadius:30 }}>
+        <CardImage style={{ flex: 2, border: 1 }}>
+          <Image
+            style={{ width: 60, height: 60, borderRadius: 30, margin: 3, borderColor: "#d6d4d4", borderWidth: 3 }}
+            source={{ uri: `${item.profile_url}` }}
+          />
+        </CardImage>
+        <View style={{flex:3}}>
+          <Text style={{ fontSize: 16, alignSelf: 'flex-start', margin:5 }}>{item.message}</Text>
+          <Text style={{ fontSize: 12, alignSelf:'flex-end',color:'#444343', marginRight:10 }}>{moment(item.timestamp).format('MMMM Do, h:mm:ss a')}</Text>
         </View>
       </View>
-    )
-  }
-
-  FlatListItemSeparator = () => {
-    return (
-      <View
-        style={{
-          alignSelf: 'center',
-          height: 1,
-          width: "90%",
-          backgroundColor: "#d6d4d4",
-        }}
+    ) : 
+    (< View style={{ backgroundColor: 'white', flexDirection: "row", justifyContent: 'flex-end', margin: 3, borderTopLeftRadius:30, borderBottomLeftRadius:30 }}>
+      <View style={{flex:3}}>
+        <Text style={{ fontSize: 16, alignSelf: 'flex-end', margin:5, marginRight:20 }}>{item.message}</Text>
+        <Text style={{ fontSize: 12, alignSelf:'flex-start',color:'#444343', marginLeft:10 }}>{moment(item.timestamp).format('MMMM Do, h:mm:ss a')}</Text>
+      </View>
+      <CardImage style={{ flex: 2, border: 1 }}>
+      <Image
+        style={{ width: 60, height: 60, borderRadius: 30, margin: 3, borderColor: "#d6d4d4", borderWidth: 3 }}
+        source={{ uri: `${item.profile_url}` }}
       />
-    );
+    </CardImage>
+    </View>)
   }
 
   chatMessages = (msgs) => {
-    if (msgs && msgs.length > 0) {
-      return (<FlatList
-        ItemSeparatorComponent={this.FlatListItemSeparator}
-        data={msgs}
-        extraData={msgs}
-        keyExtractor={(feed) => { feed.timestamp }}
-        renderItem={this.renderItem}
-      />)
+    if(msgs && msgs.length > 0) {
+      return(<FlatList          
+      data={msgs}
+      extraData={msgs}
+      keyExtractor={(feed)=>{feed.timestamp}}
+      renderItem={this.renderItem}
+    />)
     } else {
       return (
         <Text> No Previous chat</Text>
       )
-    }
+    } 
   }
 
   addNewFeed = () => {
-    console.log("PROPS::", this.props)
     const db = firebase.database();
-    const { user: { id }, navigation: { state: { params } } } = this.props
-    if (this.state.text) {
-      console.log("data", this.state.text)
+    const { user: {id}, navigation: { state: { params } }} = this.props
+    if(this.state.text) {
       db.ref('/messages').push().set({
         recipent: params.id,
         sender: id,
         message: this.state.text,
         timestamp: Date.now(),
-      });
+    });
     }
     this.setState({
       text: ''
@@ -118,24 +139,28 @@ class Chat extends React.Component {
   }
 
   render() {
-
-    const { text, messages } = this.state
-    const iconColor = text ? 'black' : 'grey'
-    console.log("mgs in render::", messages)
+    
+    const { text, messages, recivedMessage } = this.state
+    if(recivedMessage) {
+      
+      console.log("datata is::", messages.concat(recivedMessage))
+    }
+    
+    const iconColor = text ? 'black': 'grey' 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-        <View style={{ backgroundColor: "white", height: 60, margin: 10, flexDirection: 'row', justifyContent: "flex-start", alignSelf: "flex-end", borderColor: 'grey', borderWidth: 2, borderRadius: 30 }}>
-          <TextInput
-            placeholder="New Message"
-            style={{ backgroundColor: "white", width: "85%", marginLeft: 20, height: 53, alignSelf: 'center' }}
-            onChangeText={(text) => this.setState({ text })}
-            value={text} />
-          <AddIcon style={{ alignSelf: "center", color: iconColor }} name='send' size={30} onPress={this.addNewFeed} />
+      <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+        <View style={{backgroundColor: "white", height:60, margin:10, flexDirection: 'row', justifyContent:"flex-start", alignSelf:"flex-end", borderColor: 'grey', borderWidth: 2, borderRadius: 30}}>
+          <TextInput 
+          placeholder="New Message"
+          style={{ backgroundColor: "white", width:"85%", marginLeft:20, height:53, alignSelf: 'center' }}
+          onChangeText={(text) => this.setState({text})}
+          value={text}/> 
+          <AddIcon style={{alignSelf:"center", color: iconColor}}  name='send' size={30} onPress={this.addNewFeed} />
         </View>
         {
-          this.chatMessages(messages)
+          recivedMessage && this.chatMessages(messages.concat(recivedMessage))
         }
-
+        
       </View>
     );
   }
