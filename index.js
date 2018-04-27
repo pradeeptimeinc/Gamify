af4651131499de0fbe28eef9aeb2b5d081b394a1
Pradeep Sharma <pradeep.sharma@timeinc.com>
2018-04-27 05:35:58 +0530

    removed warnings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 // import App from './App';
 import App from './src';
-
+// to disable warnings uncomment below line
+console.disableYellowBox = true;
 AppRegistry.registerComponent('Gamify', () => App);
